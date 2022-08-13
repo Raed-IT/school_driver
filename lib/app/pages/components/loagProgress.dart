db31b3fd0c294e4ac2c1724b318bac65075a3824
Raed-IT <RaedSwanIT@gmail.com>
2022-08-13 10:03:43 +0300

    Initial commit

---
+++ lib/app/pages/components/loagProgress.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class LoadProgress extends StatelessWidget {
+  const LoadProgress({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const  CircularProgressIndicator();
+  }
+}
+
---
+++ lib/app/route/getPages.dart
@@ -0,0 +1,8 @@
+
+import 'package:get/get.dart';
+
+class GetPagesProvider {
+  static List<GetPage> getPages = [
+
+  ];
+}
