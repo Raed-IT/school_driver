496e8ac402d3a78d1d4f00b6b81bb4e1e872c0f4
Raed-IT <RaedSwanIT@gmail.com>
2022-09-18 13:24:39 +0300

    change map type

--- lib/app/pages/map_screen/map_screen.dart
+++ lib/app/pages/map_screen/map_screen.dart
@@ -12,23 +12,24 @@ class MapScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: GoogleMap(
-      //Map widget from google_maps_flutter package
-      zoomGesturesEnabled: true,
-      //enable Zoom in, out on map
-      initialCameraPosition: CameraPosition(
-        //innital position in map
-        target: studentsPosition,
-        //initial position
-        zoom: 17.0, //initial zoom level
+      body: GoogleMap(
+        //Map widget from google_maps_flutter package
+        zoomGesturesEnabled: true,
+        //enable Zoom in, out on map
+        initialCameraPosition: CameraPosition(
+          //innital position in map
+          target: studentsPosition,
+          //initial position
+          zoom: 17.0, //initial zoom level
+        ),
+        markers: marker,
+        //markers to show on map
+        mapType: MapType.hybrid,
+        //map type
+        onMapCreated: (controller) {
+          //method called when map is created
+        },
       ),
-      markers: marker,
-      //markers to show on map
-      mapType: MapType.satellite,
-      //map type
-      onMapCreated: (controller) {
-        //method called when map is created
-      },
-    ));
+    );
   }
 }
